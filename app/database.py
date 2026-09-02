6529734229ef51433afbcc8bad3c25b8be26b61b
Bruno Barreto <brunobarretox@gmail.com>
2026-09-02 16:46:08 -0300

    implementação da class CategoriaService + Modificações na nomenclatura em database

--- app/database.py
+++ app/database.py
@@ -2,10 +2,10 @@ import sqlite3
 
 
 class ConexaoDB:
-    def __init__(self, caminho_sql='db_solid.sqlite3'):
-        self.caminho_sql = caminho_sql
+    def __init__(self, caminho_banco='db_solid.sqlite3'):
+        self.caminho_banco = caminho_banco
 
     def obter_conexao(self):
-        conexao = sqlite3.connect(self.caminho_sql)
+        conexao = sqlite3.connect(self.caminho_banco)
         conexao.execute("PRAGMA foreign_keys = ON;")
         return conexao
--- app/services.py
+++ app/services.py
@@ -1,13 +1,65 @@
 from .database import ConexaoDB
 
+
 class CategoriaService:
     def __init__(self):
         self.conexao_bd = ConexaoDB()
-        
-    # métodos CRUD
+
+    def listar(self):
+        conexao = self.conexao_bd.obter_conexao()
+        try:
+            return conexao.cursor().execute(
+                "SELECT id, descricao FROM Categoria ORDER BY descricao"
+            ).fetchall()
+        finally:
+            conexao.close()
+
+    def buscar_por_id(self, id):
+        conexao = self.conexao_bd.obter_conexao()
+        try:
+            return conexao.cursor().execute(
+                "SELECT id, descricao FROM Categoria WHERE id = ?",
+                (int(id),)
+            ).fetchone()
+        finally:
+            conexao.close()
+
+    def incluir(self, descricao):
+        conexao = self.conexao_bd.obter_conexao()
+        try:
+            conexao.cursor().execute(
+                "INSERT INTO Categoria (descricao) VALUES (?)",
+                (descricao,)
+            )
+            conexao.commit()
+        finally:
+            conexao.close()
+
+    def alterar(self, id, descricao):
+        conexao = self.conexao_bd.obter_conexao()
+        try:
+            conexao.cursor().execute(
+                "UPDATE Categoria SET descricao = ? WHERE id = ?",
+                (descricao, int(id))
+            )
+            conexao.commit()
+        finally:
+            conexao.close()
+
+    def excluir(self, id):
+        conexao = self.conexao_bd.obter_conexao()
+        try:
+            conexao.cursor().execute(
+                "DELETE FROM Categoria WHERE id = ?",
+                (int(id),)
+            )
+            conexao.commit()
+        finally:
+            conexao.close()
+
 
 class ProdutoService:
     def __init__(self):
-            self.conexao_bd = ConexaoDB()
-    
+        self.conexao_bd = ConexaoDB()
+
     # métodos CRUD
